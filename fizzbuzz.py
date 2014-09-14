26f8f31654973dc22e0ff15c277355f797e0eeba
Andrew McGuire <mcguirea@gmail.com>
2014-09-13 21:05:34 -0700

    fizzbuzz v1

---
+++ fizzbuzz.py
@@ -0,0 +1,26 @@
+n = 15
+#my_input = raw_input("How high do you want to count?")
+#print "Fizz buzz counting up to " + str(n)
+
+fizzbuzz = []
+
+i = 1
+
+while i <= n: 
+	div3 = i % 3
+	div5 = i % 5
+	
+	if (div3 == 0) and (div5 == 0):
+		value = "fizz buzz"
+
+	elif (div3 == 0) and (div5 != 0): 
+		value = "fizz"
+
+	elif (div5 == 0) and (div3 !=0): 
+		value = "buzz"
+	else: 
+		value = i
+	fizzbuzz.append(value)
+	i = i + 1
+
+print fizzbuzz
